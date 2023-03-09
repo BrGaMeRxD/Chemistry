7413ee00abba49ce6a99e66616a30e0ca5ef721a
BrGaMeRxD <89182347+BrGaMeRxD@users.noreply.github.com>
2023-03-09 23:25:31 +0330

    test

--- backend/main/home/views.py
+++ backend/main/home/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def Home(request):
-    return render(request,'home/base.html')
+    return render(request,'home/home.html',context={'title':'Home'})
+
+def Contact(request):
+    return render(request,'home/contact.html',context={'title':'Contact'})
